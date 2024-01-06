9373657ff1fb2b9240ad5456c7fb2ccf66b8a961
JungJiwon <jiwon@JungJiwonui-MacBookAir.local>
2024-01-07 01:12:17 +0900

    영화

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import './App.css';
+import Movie from './components/movie';
+import { dummy } from './movieDummy';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <div className="app-container">
+        {
+          dummy.results.map((item) => {
+            return(
+              <Movie 
+                title={item.title}
+                poster_path={item.poster_path}
+                vote_average={item.vote_average}
+              />
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
